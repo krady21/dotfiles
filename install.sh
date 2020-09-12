8703c6c22079602c01f8cc9c0c91ed0f4f9fcac7
Bogdan Grigoruta <bogdangrigoruta@gmail.com>
2020-09-12 14:19:29 +0300

    Add gitconfig file

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ echo "61 62" | bash -c  "$(wget -qO- https://git.io/vQgMr)"
 cp .vimrc ~/.vimrc
 cp .tmux.conf ~/.tmux.conf
 cp -r zathurarc ~/.config/zathura/zathurarc
+cp -r init.vim ~/.config/nvim/init.vim
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
