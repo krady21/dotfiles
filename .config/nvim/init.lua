e5649657b915871814e07ae21659b8c0c5460789
Bogdan Grigoruță <bogdangrigoruta@gmail.com>
2021-05-27 20:31:23 +0300

    Move files to $XDG_CONFIG_HOME, bump init.lua
    init.lua:
    - start using folds
    - move compe inside the on_attach properly

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,26 +2,29 @@ local g, o, w, b = vim.g, vim.o, vim.wo, vim.bo
 local cmd, fn = vim.cmd, vim.fn
 local map = vim.api.nvim_set_keymap
 
-cmd 'packadd paq-nvim'
 local paq = require'paq-nvim'.paq
 
-paq {'savq/paq-nvim', opt = true}
+paq {'savq/paq-nvim'}
 paq {'neovim/nvim-lspconfig'}
+paq {'hrsh7th/nvim-compe'}
 paq {'junegunn/fzf', run = fn['fzf#install']}
 paq {'junegunn/fzf.vim'}
 paq {'justinmk/vim-dirvish'}
 paq {'tommcdo/vim-exchange'}
 paq {'tpope/vim-commentary'}
+paq {'tpope/vim-repeat'}
 paq {'tpope/vim-sleuth'}
 paq {'tpope/vim-surround'}
 paq {'wellle/targets.vim'}
 
 g.mapleader = ' '
-b.undofile = true
 w.wrap = false
 w.number = true
 w.relativenumber = true
 w.breakindent = true
+w.foldenable = false
+w.foldmethod = 'indent'
+o.path = '.,,**'
 o.showbreak = '↳'
 o.laststatus = 1
 o.report = 0
@@ -29,8 +32,9 @@ o.shiftwidth = 4
 o.softtabstop = 4
 o.expandtab = true
 o.hidden = true
-o.swapfile = false
-o.modeline = false
+b.undofile = true
+b.swapfile = false
+b.modeline = false
 o.splitbelow = true
 o.splitright = true
 o.lazyredraw = true
@@ -40,27 +44,23 @@ o.smartcase = true
 o.ignorecase = true
 o.virtualedit = 'block,insert'
 o.clipboard = 'unnamedplus'
-o.completeopt = 'menuone,noinsert,noselect'
+o.inccommand = 'nosplit'
+o.completeopt = 'menuone,noselect'
 o.statusline = '%< %f %m%r%w%=%l/%-6L %3c '
-o.listchars = 'tab:> ,trail:∙,nbsp:•'
+o.listchars = 'tab:| ,trail:∙,nbsp:•'
 o.dictionary = '/usr/share/dict/words'
 o.wildignore = '*/.git/*,*/tmp/*,*.swp,*.o,*.pyc'
 o.diffopt = 'internal,filler,closeoff,indent-heuristic,algorithm:patience'
-o.shortmess = vim.o.shortmess .. 'c'
+b.nrformats = 'bin,hex,alpha'
+o.shortmess = o.shortmess .. 'c'
 
-o.background = 'dark'
 o.termguicolors = true
-cmd 'colorscheme happy_hacking'
+g.colors_name = 'happy_hacking'
 
 if fn['executable']('rg') then
   o.grepprg = 'rg --no-heading --vimgrep'
   o.grepformat = '%f:%l:%c:%m'
 end
-
-cmd 'cnoreabbrev Q q'
-cmd 'cnoreabbrev W w'
-cmd 'cnoreabbrev Wq wq'
-cmd 'cnoreabbrev Qa qa'
 
 map('', 'H', '^', {noremap = true})
 map('', 'L', '$', {noremap = true})
@@ -71,47 +71,56 @@ map('i', '[<CR>', '[<CR>]<C-o>O', {noremap = true})
 map('n', 'Y', 'y$', {noremap = true})
 map('n', 'gp', '`[v`]', {noremap = true})
 
+map('n', '<leader><leader>', '<C-^>', {noremap = true})
+
 map('n', '<C-j>', ':cnext<CR>', {noremap = true, silent = true})
 map('n', '<C-k>', ':cprev<CR>', {noremap = true, silent = true})
 
-map('n', '<leader><leader>', '<C-^>', {noremap = true})
-
 map('n', '<leader>dt', ':windo diffthis<CR>', {noremap = true, silent = true})
-map('n', '<leader>do', ':windo diffoff!<CR>', {noremap = true, silent = true})
+map('n', '<leader>do', ':windo diffoff<CR>', {noremap = true, silent = true})
 
 map('n', '<F1>', ':set invrnu<CR>', {noremap = true, silent = true})
 map('n', '<F2>', ':set invspell<CR>', {noremap = true, silent = true})
 map('n', '<F3>', ':set invwrap<CR>', {noremap = true, silent = true})
 map('n', '<F4>', ':set invlist<CR>', {noremap = true, silent = true})
 
-map('n', '<leader>f', [[(len(system('git rev-parse')) ? ':Files' : ':GFiles')."\<CR>"]], {noremap = true, expr = true})
-map('n', '<leader>F', ':GFiles?<CR>', {noremap = true})
-map('n', '<leader>s', ':Rg<CR>', {noremap = true})
+map('n', '<leader>f', ':Files<CR>', {noremap = true})
+map('n', '<leader>F', ':GFiles<CR>', {noremap = true})
+map('n', '<leader>G', ':GFiles?<CR>', {noremap = true})
 map('n', '<leader>h', ':History<CR>', {noremap = true})
+map('n', '<leader>s', ':Rg<CR>', {noremap = true})
 
-cmd [[command! Whitespace let b:save = winsaveview() | keeppatterns %s/\s\+$//e | call winrestview(b:save)]]
-cmd [[command! -range Blame echo join(systemlist("git -C " . shellescape(expand('%:p:h')) . " blame -L <line1>,<line2> " . expand('%:t')), "\n")]]
+cmd('cnoreabbrev Q q')
+cmd('cnoreabbrev W w')
+cmd('cnoreabbrev Wq wq')
+cmd('cnoreabbrev Qa qa')
 
-vim.api.nvim_exec([[
+cmd([[command! Whitespace let b:save = winsaveview() | keeppatterns %s/\s\+$//e | call winrestview(b:save)]])
+cmd([[command! -range Blame echo join(systemlist("git -C " . shellescape(expand('%:p:h')) . " blame -L <line1>,<line2> " . expand('%:t')), "\n")]])
+cmd([[command! Sbd b#|bd#]])
+
+cmd([[
 augroup Personal
   autocmd!
   autocmd FileType cpp,java setlocal commentstring=//\ %s
   autocmd BufWritePre * if '<afile>' !~ '^scp:' && !isdirectory(expand('<afile>:h')) | call mkdir(expand('<afile>:h'), 'p') | endif
-  autocmd TextYankPost * silent! lua vim.highlight.on_yank {timeout=200}
+  autocmd TextYankPost * silent! lua vim.highlight.on_yank {timeout = 200}
 augroup END
-]], false)
-
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    underline = false,
-    signs = true,
-    virtual_text = false,
-  }
-)
+]])
 
 local on_attach = function(client, bufnr)
   local function buf_map(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
   local function buf_opt(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  require('compe').setup({
+    preselect = 'disable';
+    source = {
+      nvim_lsp = true;
+    };
+  }, bufnr)
+
+  buf_opt('omnifunc', 'v:lua.vim.lsp.omnifunc')
+  cmd('setlocal signcolumn=yes')
 
   local opts = { noremap=true, silent=true }
   buf_map('n', '<leader>gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
@@ -127,10 +136,15 @@ local on_attach = function(client, bufnr)
   buf_map('n', '<leader>e',  '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
   buf_map('n', '[g',         '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
   buf_map('n', ']g',         '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-
-  buf_opt('omnifunc', 'v:lua.vim.lsp.omnifunc')
-  buf_map('i', '<C-n>', '<C-x><C-o>', opts)
 end
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    underline = false,
+    signs = true,
+    virtual_text = false,
+  }
+)
 
 local nvim_lsp = require('lspconfig')
 local servers = { "clangd", "rust_analyzer", "pyright", "texlab", "hls", "gopls" }
